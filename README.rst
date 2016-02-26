a84d5b8d8f4f7e3d45417af0e490b930a55238ce
rab <badger.ea@gmail.com>
2016-02-26 14:37:08 +0000

    Added simple tool functionality. End users can now run the tool to train/generate text.

--- README.rst
+++ README.rst
@@ -2,3 +2,19 @@ Slant-Tau
 =========
 
 My dissertation project, a poetry generator. Early WIP.
+
+Usage
+=====
+Although the focus of this repository is for the library,
+an end-user tool is included for demonstrative purposes.
+It is not advised to use this tool for large amounts of generation as it takes some time to launch and/or import/train on data each time it is launched.
+
+Run the program as *racket main.rkt <args>*.
+An executable can be produced by running *raco exe main.rkt*
+
+The arguments are mostly commands and can be chained. The following commands are available:
+
+  - -t, --train <file>: Train on an input corpus.
+  - -i, --import <file>: Import some previously exported training data.
+  - -e, --export <file>: Export currently loaded training data.
+  - -g, --generate <num-words>: Generate *num-words* number of words of text.
--- main.rkt
+++ main.rkt
@@ -15,6 +15,12 @@
 (module+ test
   (require rackunit))
 
+(module+ main
+  (require racket/cmdline
+           racket/list
+           racket/match
+           "markov.rkt"))
+
 ;; Notice
 ;; To install (from within the package directory):
 ;;   $ raco pkg install
@@ -38,10 +44,71 @@
 
 ;; Code here
 
+(define program-version '(0 0 1))
+(define program-name "slant-tau")
+(define (print-version)
+  (apply fprintf `(,(current-error-port)
+                   "~a version ~a.~a.~a~n"
+                   ,program-name
+                   ,@program-version))) ; expand version
+
+(define (exit-with-error msg)
+  (print msg (current-error-port))
+  (exit 1))
+
 (module+ test
   ;; Tests to be run with raco test
   )
 
 (module+ main
   ;; Main entry point, executed when run with the `racket` executable or DrRacket.
+  (define operation-list
+    (let* ((oplist (make-parameter '()))
+           (add-op (λ (op-name file-name)
+                     (oplist (cons (cons op-name file-name)
+                                   (oplist))))))
+      (command-line
+       #:program program-name
+       #:once-each
+       [("-V" "--version") "Prints the program version."
+                           (begin
+                             (print-version)
+                             (exit))]
+       #:multi
+       [("-t" "--train")    file-name
+                            "Train on given file."
+                            (add-op 'train file-name)]
+       [("-g" "--generate") num-words
+                            "Generate a number of words."
+                            (add-op 'generate num-words)]
+       [("-i" "--import")   file-name
+                            "Import training data from file."
+                            (add-op 'import file-name)]
+       [("-e" "--export")   file-name
+                            "Export training data to file."
+                            (add-op 'export file-name)]
+       
+       #:handlers (λ (args) (reverse (oplist)))
+                  '()))) ; Inelegant, but required
+  (let ([*word-hash* (make-hash)])
+    (for ((op-pair operation-list))
+      (let ((op (car op-pair))
+            (arg (cdr op-pair)))
+        (match op
+          ['train
+           (with-input-from-file arg
+             (λ ()
+               (train *word-hash* 5)))]
+          ['generate
+           (display (generate *word-hash*
+                              3
+                              (string->number arg)))]
+          ['import
+           (merge-word-hashes *word-hash*
+                              (call-with-input-file arg
+                                (λ (port) (read port))))]
+          ['export
+           (call-with-output-file arg
+             (λ (port)
+               (write *word-hash* port)))]))))
   )
